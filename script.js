f7517235a7ea707fe9e0f484b8a251e077408b0a
Chris Natale <natale565@icloud.com>
2025-01-06 22:06:50 -0500

    updates to Js file

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
-const numberInput = document.getElementById("number-input");
-const convertBtn = document.getElementById("convert-btn");
-const result = document.getElementById("result");
+const numberInput = document.getElementById('number');
+const convertButton = document.getElementById('convert-btn');
+const output = document.getElementById('output');
+
+const numeral = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+];
+
+numberInput.addEventListener('keydown', (e) =>{
+    if(e.key === 'Enter'){
+        convertButton.click();
+    }
+} );
+
+convertButton.addEventListener('click', () => {
+const number = numberInput.value;
+
+if(!number){
+    output.innerText = 'Please enter a valid number';
+    return;
+}else if(number < 1){
+    output.innerText = "Please enter a number greater than or equal to 1";
+    return;
+}else if(number > 3999){
+output.innerText = "Please enter a number less than or equal to 3999";
+return;
+}
+});
