89b96d01bec588a0850ed18c7491f8b426b5a77f
Chris Natale <natale565@icloud.com>
2025-01-06 22:31:13 -0500

    fix to js so it doesnt clear right away

--- script.js
+++ script.js
@@ -1,51 +1,53 @@
-const numberInput = document.getElementById('number');
-const convertButton = document.getElementById('convert-btn');
-const output = document.getElementById('output');
+const numberInput = document.getElementById("number");
+const convertButton = document.getElementById("convert-btn");
+const output = document.getElementById("output");
 
 const numeral = [
-    ['M', 1000],
-    ['CM', 900],
-    ['D', 500],
-    ['CD', 400],
-    ['C', 100],
-    ['XC', 90],
-    ['L', 50],
-    ['XL', 40],
-    ['X', 10],
-    ['IX', 9],
-    ['V', 5],
-    ['IV', 4],
-    ['I', 1]
+  ["M", 1000],
+  ["CM", 900],
+  ["D", 500],
+  ["CD", 400],
+  ["C", 100],
+  ["XC", 90],
+  ["L", 50],
+  ["XL", 40],
+  ["X", 10],
+  ["IX", 9],
+  ["V", 5],
+  ["IV", 4],
+  ["I", 1],
 ];
 
-numberInput.addEventListener('keydown', (e) =>{
-    if(e.key === 'Enter'){
-        convertButton.click();
-    }
-} );
+numberInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    convertButton.click();
+  }
+});
 
-convertButton.addEventListener('click', () => {
-let number = numberInput.value;
+convertButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  let number = parseInt(numberInput.value, 10);
 
-if(!number){
-    output.innerText = 'Please enter a valid number';
+  if (!number) {
+    output.innerText = "Please enter a valid number";
     return;
-}else if(number < 1){
+  } else if (number < 1) {
     output.innerText = "Please enter a number greater than or equal to 1";
     return;
-}else if(number > 3999){
-output.innerText = "Please enter a number less than or equal to 3999";
-return;
-} else {
-    let result = '';
+  } else if (number > 3999) {
+    output.innerText = "Please enter a number less than or equal to 3999";
+    return;
+  } else {
+    let result = "";
 
-    for(const [roman, numbers] of numeral){
-        while (numberInput >= numbers) {
-          result += roman;
-          number -= number;
-        }
+    for (const [roman, value] of numeral) {
+      while (number >= value) {
+        result += roman;
+        number -= value;
+      }
     }
-    
+
     output.innerText = result;
-}
+  }
 });
